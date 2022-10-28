1d85abdd24b5ecc2befc9515d13c6a22ce3900df
cyan <yozdaz@gmail.com>
2022-10-28 17:11:08 +0900

    scrapv2

--- scraper.py
+++ scraper.py
@@ -1,29 +1,27 @@
 # Utility function for scraping
 import requests, json
-from requests import request
 from bs4 import BeautifulSoup
 import re
-import PCH
+from PCH import pch
 
 class ScrapImg:
-    def __init__(self, catdict):
-        self.pch = PCH.pch()
+    def __init__(self, catdict : dict):
         self.catdict = catdict
-    def NaverShopping(self, query : str, path : str, catdict : dict):
+    def NaverShopping(self, query : str, path : str):
         if path[-1] != '/':
             path = path + '/'
-        self.pch.nsp['query'] = self.pch.nsp['origQuery'] = query
+        pch.nsp['query'] = pch.nsp['origQuery'] = query
         cate = catdict[query]
-        self.pch.nsp['pagingIndex'] = 1
+        pch.nsp['pagingIndex'] = 1
 
-        while self.pch.nsp['pagingIndex'] != None:
-            resp = requests.get(self.pch.nss, params=self.pch.nsp, cookies=self.pch.nsc, headers=self.pch.nsh)
+        while pch.nsp['pagingIndex'] != None:
+            resp = requests.get(pch.nss, params=pch.nsp, cookies=pch.nsc, headers=pch.nsh)
             temp = json.loads(resp.text)
             i = 1
-            while i <= int(self.pch.nsp['pagingSize']):
+            while i <= int(pch.nsp['pagingSize']):
                 for _ in temp['shoppingResult']['products']:
                     url = _['imageUrl']
-                    urlresp = requests.get(url)
+                    imgresp = requests.get(url)
                     a = re.search(r'(\w+)(?:\s)(.+)', _['productTitle'])
                     try:
                         brand_name = a.group(1)
@@ -31,44 +29,33 @@ class ScrapImg:
                     except:
                         pass
 
-                    j = (self.pch.nsp['pagingIndex'] - 1) * 40 + i
+                    j = (pch.nsp['pagingIndex'] - 1) * 40 + i
                     fileName = f'{cate}-{brand_name}-{product_name}-{j}' + '.jpg'
 
                     try:
                         with open(path + fileName, 'wb') as fp:
-                            fp.write(urlresp.content)
+                            fp.write(imgresp.content)
                     except:
                         pass
                     i += 1
-            self.pch.nsp['pagingIndex'] += 1
+            pch.nsp['pagingIndex'] += 1
 
-    def WeMakePrice(self):
-        # In[ ]:
+    def WeMakePrice(self, query : str, path : str):
+        if path[-1] != '/':
+            path = path + '/'
+        pch.wmpp['keyword'] = query
+        cate = self.catdict[query]
+        pch.wmpp['page'] = 1
 
-        seed = 'https://search.wemakeprice.com/api/wmpsearch/api/v3.0/wmp-search/search.json'
-        params = {
-            'searchType': 'DEFAULT',
-            'search_cate': 'top',
-            'keyword': None,
-            'isRec': 1,
-            '_service': 5,
-            '_type': 3,
-            'page': None
-        }
-
-        params['keyword'] = input('검색어:')
-        cate = int(input('카테고리 번호:'))
-        params['page'] = 1
-
-        while params['page'] != None:
-            resp = get(seed, params=params, headers=headers)
+        while pch.wmpp['page'] != None:
+            resp = requests.get(pch.wmps, params=pch.wmpp, headers=pch.wmph)
             resp.headers
             dom = BeautifulSoup(resp.text, 'html.parser')
             temp = resp.json()
             i = 1
             for _ in temp['data']['deals']:
                 url = _['largeImgUrl']
-                urlresp = get(url)
+                imgresp = requests.get(url)
                 a = re.search(r'(\w+)(?:\s)([\w\s]+)(?:\s)([독립|본넬]?)', _['dispNm'])
                 try:
                     brand_name = a.group(1)
@@ -76,13 +63,13 @@ class ScrapImg:
                 except:
                     pass
 
-                j = (params['page'] - 1) * 82 + i
+                j = (pch.wmpp['page'] - 1) * 82 + i
                 fileName = f'{cate}-{brand_name}-{product_name}-{j}' + '.jpg'
 
                 try:
-                    with open('./bed/' + fileName, 'wb') as fp:
-                        fp.write(urlresp.content)
+                    with open(path + fileName, 'wb') as fp:
+                        fp.write(imgresp.content)
                 except:
                     pass
                 i += 1
-            params['page'] += 1
+            pch.wmpp['page'] += 1
