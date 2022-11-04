7c09c0aebbb4f7761e2b81ef2f504e6c23c935e4
cyan <yozdaz@gmail.com>
2022-11-04 17:33:35 +0900

    limit added

--- run_scrap.py
+++ run_scrap.py
@@ -1,8 +1,21 @@
 from scraper import ScrapImg
 from PCH import pch
-catdict = {'침대' : 1, '책상' : 2, '의자' : 3, '서랍장' : 4}
 
+catdict = {'모션베드' : 1}
 scraper = ScrapImg(catdict)
+scraper.NaverShopping('모션베드', 'D:\Workspace\SW_academy\Project1images')
+scraper.WeMakePrice('모션베드', 'D:\Workspace\SW_academy\Project1images')
 
-# scraper.NaverShopping('책상', 'D:\Workspace\SW_academy\Project1images')
-# scraper.WeMakePrice('책상', 'D:\Workspace\SW_academy\Project1images')
+# def run(path = None, query = None, catdict = None, site = None):
+#     scraper = ScrapImg(catdict)
+#     if site == 'NaverShopping':
+#         scraper.NaverShopping(query, path)
+#     if site == 'WeMakePrice':
+#         scraper.WeMakePrice(query, path)
+#
+# def main(path = None, query = None, catdict = None, site = None):
+#     if path and query and catdict and site:
+#         run(path=path,query=query,catdict=catdict,site=site)
+#
+# if __name__ == "__main__":
+#     main()
